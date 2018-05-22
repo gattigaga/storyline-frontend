49f097b84be1386baef28fd81d7643aebada7783
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-05-22 19:39:00 +0700

    Install fonts

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import "normalize.css";
+import "typeface-roboto";
+import "typeface-open-sans";
+
 ReactDOM.render(<App />, document.getElementById("root"));
